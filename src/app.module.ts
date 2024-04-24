da524f2ea3dcebd6dcdd17d97b68074f777cac95
FrancescaImmediato <francesca.immediato.cohort2211@gmail.com>
2024-04-23 21:00:24 -0700

    working towards creating db and schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {User} from './user/entities/user.entity'
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'Francesca',
+      password: 'Remnant113!',
+      database: 'timetrackerappdata1',
+      entities: [User],
+      synchronize: false,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
